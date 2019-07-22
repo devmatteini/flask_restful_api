a59f50359683a7f105529066e36b9b40ffc108d8
devmatteini <dev.matteini@gmail.com>
2019-07-22 02:29:08 +0200

    API endpoints update.

--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,17 @@
+from flask import jsonify
 from app import app
+from app.models import Championship, championship_schema, championships_schema
 
 
 @app.route('/api/championships', methods=['GET'])
 def get_championships():
-    pass
+    all_championships = Championship.query.all()
+    result = championships_schema.dump(all_championships)
+
+    return jsonify(result)
 
 
 @app.route('/api/championship/<int:id>', methods=['GET'])
 def get_championship(id):
-    pass
+    championship = Championship.query.filter_by(id_championship=id).first()
+    return championship_schema.jsonify(championship)
